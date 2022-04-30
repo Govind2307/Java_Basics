ebd2b6c5a2f3b5383a025db54f785692a838526c
hp <govindpurohit@gmail.com>
2022-04-30 14:59:18 +0530

    Strings

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,62 @@
-public class Main {
+public class Main {  //class
     public static void main(String[] args)
+    //function (1st call when function is called)
     {
-        System.out.println("Hello world!");
-        System.out.println("Java Basics");
+        //First programmed
+        System.out.println("Hello world!"); // in the new line
+        System.out.println("Java Basics"); //no new line
+
+        // Methods are the function which is written under the classes (print)
+
+        //varable types
+        /*
+         * byte - 1 [-128 to 127]
+         * short - 2
+         * int - 4
+         * long - 8
+         * float - 4
+         * double - 8
+         * char - 2
+         * boolean - 1 true/false
+         */
+
+        //Variable
+        String Name = "Govind";
+        byte age = 30;
+        long phn = 7610658089L;
+        float pi = 3.14F;
+        char letter = 'G';
+        boolean isAdult = true;
+
+
+
+        //Operations on Strings
+        //Strings are immutable means we cannot change it
+        // concatenate
+
+        String name1 = "Ayush";
+        String name2 = "Sharvil";
+        String name3 = name1 + " " + name2;
+        System.out.println(name3);
+
+        //Length
+        String name = "Govi";
+        System.out.println(name);
+        System.out.println(name.length());
+
+        // charAt
+        //Java is a 0 index based language
+        String name4 = "Ayush";
+        System.out.println(name4.charAt(0));
+
+        //Replace
+        String name5 = name2.replace('a','b');
+        System.out.println(name5);
+
+        //substring
+        String a = "Ayush and naman";
+        System.out.println(a.substring(0,5)); //0 is included but 5 is not
+
+
     }
 }
